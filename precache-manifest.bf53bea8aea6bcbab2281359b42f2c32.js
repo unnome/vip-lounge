8779dba88dccf13a3d26fdcbbda793bcafca0e9e
unnome <unnome@gmail.com>
2019-10-27 19:51:39 +0100

    Updates

---
+++ precache-manifest.bf53bea8aea6bcbab2281359b42f2c32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d883f1914b03e7f11c8b3ad94ca598",
+    "url": "/vip-lounge/index.html"
+  },
+  {
+    "revision": "b4bbd1c1864a680f7702",
+    "url": "/vip-lounge/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7aa8003e503ca65eaeb4",
+    "url": "/vip-lounge/static/js/2.d7f8b8d5.chunk.js"
+  },
+  {
+    "revision": "b4bbd1c1864a680f7702",
+    "url": "/vip-lounge/static/js/main.afd79662.chunk.js"
+  },
+  {
+    "revision": "a9a3f72dae36f0d6e2fa",
+    "url": "/vip-lounge/static/js/runtime-main.de75fac9.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/vip-lounge/static/media/logo.25bf045c.svg"
+  }
+]);
